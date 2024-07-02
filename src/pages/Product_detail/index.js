4295e7df4ed8c8238e1d47a1f71a3252f1d392ef
cutu11 <96825898+cutu11@users.noreply.github.com>
2024-07-02 23:15:54 +0700

    commit4

--- src/pages/Product_detail/index.js
+++ src/pages/Product_detail/index.js
@@ -12,45 +12,64 @@ import detail8 from '~/assets/Product_detail/detail8.jpg';
 import detail9 from '~/assets/Product_detail/detail9.jpg';
 import detail10 from '~/assets/Product_detail/detail10.jpg';
 
-
 const cx = classNames.bind(styles);
 
 function Product_detail() {
     const [soLuong, setSoLuong] = useState(1);
+    const [mainImage, setMainImage] = useState(detail1);
+    const [selectedColor, setSelectedColor] = useState('Navy');
+    const [selectedSize, setSelectedSize] = useState('M');
 
     const thayDoiSoLuong = (thayDoi) => {
         setSoLuong((soLuongTruoc) => Math.max(1, soLuongTruoc + thayDoi));
     };
 
+    const handleThumbnailClick = (image) => {
+        setMainImage(image);
+    };
+
+    const handleColorClick = (color) => {
+        setSelectedColor(color);
+    };
+
+    const handleSizeClick = (size) => {
+        setSelectedSize(size);
+    };
+
     return (
         <div className={cx('product-page')}>
             <div className={cx('image-gallery')}>
-                <img src={detail1} alt="Main Product" className={cx('main-image')} />
+                <img src={mainImage} alt="Main Product" className={cx('main-image')} />
                 <div className={cx('thumbnail-list')}>
                     <img
                         src={detail7}
                         alt="Thumbnail 1"
                         className={cx('thumbnail')}
+                        onClick={() => handleThumbnailClick(detail7)}
                     />
                     <img
                         src={detail8}
                         alt="Thumbnail 2"
                         className={cx('thumbnail')}
+                        onClick={() => handleThumbnailClick(detail8)}
                     />
                     <img
                         src={detail9}
                         alt="Thumbnail 3"
                         className={cx('thumbnail')}
+                        onClick={() => handleThumbnailClick(detail9)}
                     />
                     <img
                         src={detail10}
                         alt="Thumbnail 4"
                         className={cx('thumbnail')}
+                        onClick={() => handleThumbnailClick(detail10)}
                     />
                     {/* <img
                         src={detail6}
                         alt="Thumbnail 5"
                         className={cx('thumbnail')}
+                        onClick={() => handleThumbnailClick(detail6)}
                     /> */}
                 </div>
             </div>
@@ -61,23 +80,73 @@ function Product_detail() {
                 <h4>Giá bán:</h4>
                 <h2>399.000 ₫</h2>
                 <div className={cx('color-options')}>
-                    <p>Màu sắc: Navy</p>
+                    <p>Màu sắc: {selectedColor}</p>
                     <div className={cx('color-buttons')}>
-                        <button className={cx('color-button')}>Navy</button>
-                        <button className={cx('color-button')}>Đen</button>
-                        <button className={cx('color-button')}>Trắng</button>
-                        <button className={cx('color-button')}>Cam</button>
-                        <button className={cx('color-button')}>Xanh</button>
+                        <button 
+                            className={cx('color-button', { active: selectedColor === 'Navy' })} 
+                            onClick={() => handleColorClick('Navy')}
+                        >
+                            Navy
+                        </button>
+                        <button 
+                            className={cx('color-button', { active: selectedColor === 'Đen' })} 
+                            onClick={() => handleColorClick('Đen')}
+                        >
+                            Đen
+                        </button>
+                        <button 
+                            className={cx('color-button', { active: selectedColor === 'Trắng' })} 
+                            onClick={() => handleColorClick('Trắng')}
+                        >
+                            Trắng
+                        </button>
+                        <button 
+                            className={cx('color-button', { active: selectedColor === 'Cam' })} 
+                            onClick={() => handleColorClick('Cam')}
+                        >
+                            Cam
+                        </button>
+                        <button 
+                            className={cx('color-button', { active: selectedColor === 'Xanh' })} 
+                            onClick={() => handleColorClick('Xanh')}
+                        >
+                            Xanh
+                        </button>
                     </div>
                 </div>
                 <div className={cx('size-options')}>
-                    <p>Kích thước:</p>
+                    <p>Kích thước: {selectedSize}</p>
                     <div className={cx('size-buttons')}>
-                        <button className={cx('size-button')}>M</button>
-                        <button className={cx('size-button')}>L</button>
-                        <button className={cx('size-button')}>XL</button>
-                        <button className={cx('size-button')}>2XL</button>
-                        <button className={cx('size-button')}>3XL</button>
+                        <button 
+                            className={cx('size-button', { active: selectedSize === 'M' })} 
+                            onClick={() => handleSizeClick('M')}
+                        >
+                            M
+                        </button>
+                        <button 
+                            className={cx('size-button', { active: selectedSize === 'L' })} 
+                            onClick={() => handleSizeClick('L')}
+                        >
+                            L
+                        </button>
+                        <button 
+                            className={cx('size-button', { active: selectedSize === 'XL' })} 
+                            onClick={() => handleSizeClick('XL')}
+                        >
+                            XL
+                        </button>
+                        <button 
+                            className={cx('size-button', { active: selectedSize === '2XL' })} 
+                            onClick={() => handleSizeClick('2XL')}
+                        >
+                            2XL
+                        </button>
+                        <button 
+                            className={cx('size-button', { active: selectedSize === '3XL' })} 
+                            onClick={() => handleSizeClick('3XL')}
+                        >
+                            3XL
+                        </button>
                     </div>
                 </div>
                 <div className={cx('quantity-selector')}>
